6f73a4357f28746e268bdce999652d2b8527c2f3
alexsbc88 <billycuan88@gmail.com>
2022-10-26 20:01:49 +0700

    coba coba iki, biarin ae, aku pake dummy file semua

--- r.py
+++ r.py
@@ -26,26 +26,6 @@ try:
                 return diff.days
             return None
 
-        #function frequency
-        def frequency(id_pendonor): 
-            sql_frequency = "SELECT COUNT(id_donor) FROM `donor` WHERE id_pendonor = "+id_pendonor+" GROUP BY id_pendonor"; 
-            cursor.execute(sql_frequency)
-            freq = cursor.fetchone()
-
-            if(freq is not None): 
-                return freq[0]
-            return 0
-
-        #function monetary
-        def monetary(id_pendonor):
-            sql_monetary = "SELECT max(jumlah_darah) FROM donor WHERE id_pendonor="+id_pendonor;
-            cursor.execute(sql_monetary)
-            mon = cursor.fetchone()
-
-            if(mon is not None):
-                return mon[0]
-            return 0
-
         #get all id pendonor
         sql_id_pendonor = "SELECT id_pendonor FROM `pendonor`"
         cursor.execute(sql_id_pendonor)
@@ -87,89 +67,6 @@ try:
                     normalized_recency_arr.append(None)
 
         print("r array: ", normalized_recency_arr)
-
-
-        #FREQUENCY-----------------------------
-        frequency_array = []
-        for pendonor in id_pendonor: 
-            # print("Pendonor: ", pendonor[0])
-            # print("freq: ", frequency(str(pendonor[0])))
-            frequency_array.append(frequency(str(pendonor[0])))
-
-        #get max frequency 
-        frequency_max = 0
-        for i in frequency_array: 
-            if(i is not None): 
-                if(i > frequency_max): 
-                    frequency_max = i
-
-        #get min frequency
-        frequency_min = frequency_max
-        for i in frequency_array: 
-            if(i is not None): 
-                if(i < frequency_min): 
-                    frequency_min = i
-
-        #min-max normalization to 0-5 (frequency)
-        normalized_frequency_arr = []
-        if(frequency_max == frequency_min): 
-            for i in frequency_array: 
-                normalized_frequency_arr.append(0)
-        else: 
-            for i in frequency_array: 
-                if(i is not None): 
-                    normalized_frequency_arr.append((i-frequency_min)/(frequency_max - frequency_min)*5)
-                else: 
-                    normalized_frequency_arr.append(0.0)
-
-        print("freq: ", normalized_frequency_arr)
-
-
-        #MONETARY------------------------------------------------------------
-        monetary_array = []
-        for pendonor in id_pendonor: 
-            # print("Pendonor: ", pendonor[0])
-            # print("mon: ", monetary(str(pendonor[0])))
-            monetary_array.append(monetary(str(pendonor[0])))
-
-        #get max monetary 
-        monetary_max = 0
-        for i in monetary_array: 
-            if(i is not None): 
-                if(i > monetary_max): 
-                    monetary_max = i
-
-        #get min monetary
-        monetary_min = monetary_max
-        for i in monetary_array: 
-            if(i is not None): 
-                if(i < monetary_min): 
-                    monetary_min = i
-
-        #min-max normalization to 0-5 (monetary)
-        normalized_monetary_arr = []
-        if(monetary_max == monetary_min): #kalo datanya sama, jd gabisa dicari minmax normalization nya
-            for i in monetary_array: 
-                normalized_monetary_arr.append(0)
-        else: 
-            for i in monetary_array: 
-                if(i is not None): 
-                    normalized_monetary_arr.append((i-monetary_min)/(monetary_max - monetary_min)*5)
-                else: 
-                    normalized_monetary_arr.append(0.0)
-
-        print("mon: ", normalized_monetary_arr)
-
-
-        #RFM-----------------------------------------
-        rfm_arr = []
-        for i in range(len(monetary_array)): 
-            if(normalized_recency_arr[i] is None): 
-                rfm_arr.append(0)
-            else: 
-                rfm_arr.append(normalized_recency_arr[i]+normalized_frequency_arr[i]+normalized_monetary_arr[i])
-
-        print("rfm: ", rfm_arr)
 
 except Error as e:
     print("Error while connecting to MySQL", e)
