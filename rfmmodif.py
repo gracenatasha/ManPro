6063868f6fd7f15c0b8f3dc33c2cb3dd83bf8835
alexsbc88 <billycuan88@gmail.com>
2022-11-24 02:13:07 +0700

    Update rfmmodif.py

--- rfmmodif.py
+++ rfmmodif.py
@@ -7,6 +7,16 @@ from mysql.connector import Error
 from datetime import date
 import pandas as pd
 import sys
+from sklearn.preprocessing import StandardScaler as SS # z-score standardization 
+from sklearn.cluster import KMeans, DBSCAN # clustering algorithms
+from sklearn.decomposition import PCA # dimensionality reduction
+from sklearn.metrics import silhouette_score # used as a metric to evaluate the cohesion in a cluster
+from sklearn.neighbors import NearestNeighbors # for selecting the optimal eps value when using DBSCAN
+import numpy as np
+# plotting libraries
+import matplotlib.pyplot as plt
+import seaborn as sns
+from yellowbrick.cluster import SilhouetteVisualizer
 
 
 try:
@@ -282,6 +292,83 @@ try:
         df_pendonor.pop('ID Kelurahan Rumah')
         df_pendonor.pop('ID Kelurahan Kantor')
         sorted_pendonor = df_pendonor.sort_values(by='RFM', ascending=False)
+
+        def silhouettePlot(range_, data):
+            half_length = int(len(range_)/2)
+            range_list = list(range_)
+            fig, ax = plt.subplots(half_length, 2, figsize=(15,8))
+            for _ in range_:
+                kmeans = KMeans(n_clusters=_, random_state=42)
+                q, mod = divmod(_ - range_list[0], 2)
+                sv = SilhouetteVisualizer(kmeans, colors="yellowbrick", ax=ax[q][mod])
+                ax[q][mod].set_title("Silhouette Plot with n={} Cluster".format(_))
+                sv.fit(data)
+            fig.tight_layout()
+            fig.show()
+            fig.savefig("silhouette_plot.png")
+
+        def elbowPlot(range_, data, figsize=(10,10)):
+            inertia_list = []
+            for n in range_:
+                kmeans = KMeans(n_clusters=n, random_state=42)
+                kmeans.fit(data)
+                inertia_list.append(kmeans.inertia_)
+                
+            # plotting
+            fig = plt.figure(figsize=figsize)
+            ax = fig.add_subplot(111)
+            sns.lineplot(y=inertia_list, x=range_, ax=ax)
+            ax.set_xlabel("Cluster")
+            ax.set_ylabel("Inertia")
+            ax.set_xticks(list(range_))
+            fig.show()
+            fig.savefig("elbow_plot.png")
+
+        def findOptimalEps(n_neighbors, data):
+            neigh = NearestNeighbors(n_neighbors=n_neighbors)
+            nbrs = neigh.fit(data)
+            distances, indices = nbrs.kneighbors(data)
+            distances = np.sort(distances, axis=0)
+            distances = distances[:,1]
+            plt.plot(distances)
+
+        def progressiveFeatureSelection(df, n_clusters=3, max_features=4,):
+            feature_list = list(df.columns)
+            selected_features = list()
+            # select starting feature
+            initial_feature = ""
+            high_score = 0
+            for feature in feature_list:
+                kmeans = KMeans(n_clusters=n_clusters, random_state=42)
+                data_ = df[feature]
+                labels = kmeans.fit_predict(data_.to_frame())
+                score_ = silhouette_score(data_.to_frame(), labels)
+                print("Proposed new feature {} with score {}". format(feature, score_))
+                if score_ >= high_score:
+                    initial_feature = feature
+                    high_score = score_
+            print("The initial feature is {} with a silhouette score of {}.".format(initial_feature, high_score))
+            feature_list.remove(initial_feature)
+            selected_features.append(initial_feature)
+            for _ in range(max_features-1):
+                high_score = 0
+                selected_feature = ""
+                print("Starting selection {}...".format(_))
+                for feature in feature_list:
+                    selection_ = selected_features.copy()
+                    selection_.append(feature)
+                    kmeans = KMeans(n_clusters=n_clusters, random_state=42)
+                    data_ = df[selection_]
+                    labels = kmeans.fit_predict(data_)
+                    score_ = silhouette_score(data_, labels)
+                    print("Proposed new feature {} with score {}". format(feature, score_))
+                    if score_ > high_score:
+                        selected_feature = feature
+                        high_score = score_
+                selected_features.append(selected_feature)
+                feature_list.remove(selected_feature)
+                print("Selected new feature {} with score {}". format(selected_feature, high_score))
+            return selected_features
 
         # Nyambungin ke PHP/HTML
         html_table = sorted_pendonor.to_html(classes='table table-striped') 
