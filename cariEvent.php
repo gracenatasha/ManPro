5a266809e3c1ee8056340b88253bb61aa70fbcb6
gracenatasha <c14200021@john.petra.ac.id>
2022-12-20 23:24:14 +0700

    Update cariEvent.php
    ngerapiin UI

--- cariEvent.php
+++ cariEvent.php
@@ -5,7 +5,7 @@ require_once 'functions.php';
 $dataPerPage = 10;
 $totalData = count(query("SELECT * FROM event"));
 $pageCount = ceil($totalData / $dataPerPage);
-$activePage = (isset($_GET["page"]))?$_GET["page"]:1;
+$activePage = (isset($_GET["page"])) ? $_GET["page"] : 1;
 $begin = ($dataPerPage * $activePage) - $dataPerPage;
 $dataEvent = query("SELECT * FROM event LIMIT $begin, $dataPerPage");
 
@@ -27,6 +27,8 @@ $lokasi = query("SELECT * FROM lokasi")[1];
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Cari Event</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    </link>
 </head>
 
 <body>
@@ -34,82 +36,84 @@ $lokasi = query("SELECT * FROM lokasi")[1];
     <br><br><br>
     <div class="container">
         <h1 style="text-align: center;"><span id="ourEvent"> &nbsp; E V E N T &nbsp;</span></h1>
-    <br>
-            <a type="button" class="btn btn-primary" id="createEventButton" href="insertevent.php">+ Create Event</a>
+        <br>
+        <a type="button" class="btn btn-primary" id="createEventButton" href="insertevent.php">+ Create Event</a>
 
         <br>
-    <div class="card" style="width: 18rem;">
-        <div class="card-body">
-            <form action="" method="post">
+        <div class="card my-3" style="width: 18rem;">
+            <div class="card-body">
+                <form action="" method="post">
 
-                <div class="mb-3">
-                    <label for="search_keyword" class="form-label">Cari Event</label>
-                    <input type="text" name="keyword">
-                </div>
-                <!-- bikin table untuk menunjukkan event -->
-                <!-- pada masing masing event buat button untuk input pendonor -->
-                <!-- buat modal atau page baru untuk input id pendonor dan cc darah -->
-                <button type="submit" class="btn btn-primary" name="btnSubmit">Submit</button>
+                    <div class="mb-3">
+                        <label for="search_keyword" class="form-label">Cari Event</label>
+                        <input type="text" name="keyword">
+                    </div>
+                    <!-- bikin table untuk menunjukkan event -->
+                    <!-- pada masing masing event buat button untuk input pendonor -->
+                    <!-- buat modal atau page baru untuk input id pendonor dan cc darah -->
+                    <button type="submit" class="btn btn-primary" name="btnSubmit">Submit</button>
 
-            </form>
+                </form>
+            </div>
         </div>
-    </div>
 
+        <div id="container my-3">
+            <table class="table table-striped table-bordered" border="1" cellspacing="0" cellpadding="5">
+                <thead>
+                    <tr>
 
-    <nav aria-label="...">
+                        <th>#</th>
+                        <th>Nama</th>
+                        <th>Tanggal</th>
+                        <th>Jam mulai</th>
+                        <th>Jam selesai</th>
+                        <th>Lokasi</th>
+                        <th>Action</th>
 
-        <ul class="pagination">
+                    </tr>
+                </thead>
 
-            <li class="page-item disabled">
-                <a class="page-link">Previous</a>
-            </li>
+                <?php if (empty($dataEvent)) : ?>
+                    <tr>
+                        <td colspan="7" align="center">data mahasiswa tidak ditemukan</td>
+                    </tr>
+                <?php endif; ?>
 
-            <?php for ($i = 1; $i <= $pageCount; $i++) : ?>
-                <li class="page-item"><a class="page-link" href="?page=<?= $i; ?>"><?= $i; ?></a></li>
-            <?php endfor; ?>
+                <?php $i = 1; ?>
+                <?php foreach ($dataEvent as $row) { ?>
+                    <tr>
+                        <td><?= $row["id_event"]; ?></td>
+                        <td><?= $row["nama_event"]; ?></td>
+                        <td><?= $row["tanggal_event"]; ?></td>
+                        <td><?= $row["waktu_mulai_event"]; ?></td>
+                        <td><?= $row["waktu_akhir_event"]; ?></td>
+                        <td><?= $row["id_lokasi"]; ?></td>
+                        <td><a href="inputDonor.php?id=<?php echo $row["id_event"]; ?>">Input Donor</a></td>
+                    </tr>
+                    <?php $i++; ?>
+                <?php } ?>
+            </table>
+        </div>
+        <br>
+        <!--PAGINATION-->
+        <nav aria-label="...">
 
-            <li class="page-item active" aria-current="page">
-                <a class="page-link" href="#">Next</a>
-            </li>
-        </ul>
-    </nav>
+            <ul class="pagination">
 
+                <li class="page-item disabled">
+                    <a class="page-link">Previous</a>
+                </li>
 
+                <?php for ($i = 1; $i <= $pageCount; $i++) : ?>
+                    <li class="page-item"><a class="page-link" href="?page=<?= $i; ?>"><?= $i; ?></a></li>
+                <?php endfor; ?>
 
-    <div id="container">
-        <table border="1" cellspacing="0" cellpadding="5">
-            <tr>
+                <li class="page-item active" aria-current="page">
+                    <a class="page-link" href="#">Next</a>
+                </li>
+            </ul>
+        </nav>
 
-                <th>id</th>
-                <th>Nama</th>
-                <th>Tanggal</th>
-                <th>Jam mulai</th>
-                <th>Jam selesai</th>
-                <th>Lokasi</th>
-
-            </tr>
-
-            <?php if (empty($dataEvent)) : ?>
-                <tr>
-                    <td colspan="7" align="center">data mahasiswa tidak ditemukan</td>
-                </tr>
-            <?php endif; ?>
-
-            <?php $i = 1; ?>
-            <?php foreach ($dataEvent as $row) { ?>
-                <tr>
-                    <td><?= $row["id_event"]; ?></td>
-                    <td><?= $row["nama_event"]; ?></td>
-                    <td><?= $row["tanggal_event"]; ?></td>
-                    <td><?= $row["waktu_event_mulai"]; ?></td>
-                    <td><?= $row["waktu_event_selesai"]; ?></td>
-                    <td><?= $row["id_lokasi"]; ?></td>
-                    <td><a href="inputDonor.php?id=<?php echo $row["id_event"]; ?>">Input Donor</a></td>
-                </tr>
-                <?php $i++; ?>
-            <?php } ?>
-        </table>
-    </div>
     </div>
 
     <script src="jquery-3.1.1.min.js"></script>
