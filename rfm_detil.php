ab5520350b9e0ed438913e62747ac83d1f02a728
kellyflorences <77277696+kellyflorences@users.noreply.github.com>
2022-12-20 21:32:09 +0700

    Add files via upload

---
+++ rfm_detil.php
@@ -0,0 +1,47 @@
+<?php 
+    include 'links.php';
+    $command = escapeshellcmd('python rfm.py');
+    $output = shell_exec($command);
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="icon" type="image/png" href="" sizes="20">
+        <title>RFM</title>
+        <style>
+            body{
+                /* background-color: #F0F0F0; */
+                text-align: center;
+            }
+        </style>
+    </head>
+    <body>
+        <?php include 'navbar.php';?>
+        <div class="container mt-5">
+            <h1 style="text-align: center">DETIL RFM</h1>
+            <div class="row mt-5">
+                <div class="col-6">
+                    <div class="row">
+                        <h3>Elbow Method</h3>
+                        <p>Untuk menentukan jumlah cluster</p>
+                    </div>
+                    <div class="row">
+                        <img src="elbow.png" style="width: 100%; ">
+                    </div>
+                </div>
+                <div class="col-6">
+                    <h3>Silhouette Coefficient</h3>
+                    <p><?php echo $output?></p>
+                </div>
+            </div>
+            
+            </div>
+        </div>
+        <script>
+        </script>
+    </body>
+</html>
