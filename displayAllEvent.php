2d3bb794173db506a55310973926957ed365b35e
gracenatasha <c14200021@john.petra.ac.id>
2022-12-21 00:47:18 +0700

    baru
    - nambah fitur sisi user
    - update style.css jadi merah
    - displayallevent.php pindah ke sisi user

--- displayAllEvent.php
+++ displayAllEvent.php
@@ -1,85 +1,96 @@
-
 <?php
-//session_start();
-    include 'connect.php';
-    // include 'navbar.php';
-    include 'links.php';
+include 'connection.php';
+include 'links.php';
 ?>
-<!DOCTYPE html> 
+<!DOCTYPE html>
 <html lang="en">
-    <head>
+
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.1/font/bootstrap-icons.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"> </script>
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <title>EVENT</title>
     <style>
-        .card a{
+        .card a {
             color: black !important;
             text-decoration: none !important;
         }
-        .btn:hover{
+
+        .btn:hover {
             transform: scale(1.02);
             transition: 0.5s;
             color: white;
         }
-        .card:hover{
+
+        .card:hover {
             transform: scale(1.04);
             transition: 0.3s;
         }
-        .card-header{
+
+        .card-header {
             background-color: #88000278;
         }
-        .card-body{
+
+        .card-body {
             background-color: #8800021c;
         }
     </style>
-    </head>
-    <body>
-        <?php include 'navbar.php';?>
-        <div class="container mt-5">
-            <h1 style="text-align: center; margin-top: 100px;">EVENTS</h1>
-            <div class="row justify-content-center">
-                <div class="col-6 col-md-4 col-lg-2">
-                    <a type="button" class="btn btn-dark mt-5"  href="insertevent.php" style="width: 100%;">+ Create Event</a>
-                </div>
+</head>
+
+<body>
+    <?php include 'usernavbar.php'; ?>
+    <div class="container mt-5">
+        <h1 style="text-align: center; margin-top: 100px;">EVENTS</h1>
+        <div class="row justify-content-center">
+            <div class="col-6 col-md-4 col-lg-2">
+                <a type="button" class="btn btn-dark mt-5" href="insertevent.php" style="width: 100%;">+ Create Event</a>
             </div>
-            
-            <div class="row mt-5 justify-content-center">
-                <?php
-                    $sql = "SELECT * FROM event";
-                    $result = mysqli_query($conn, $sql);
+        </div>
 
-                    while($row = mysqli_fetch_array($result)) {
-                        //echo '<a type="button" class="btn stretched-link" id="addToCartButton" href="details.php?id='. $row["id"].'&rest_name='.$_SESSION['rest_name'].'">+ Add to cart</a>';
+        <div class="row mt-5 justify-content-center">
+            <?php
+            $sql = "SELECT * FROM event";
+            $result = mysqli_query($conn, $sql);
 
-                        //echo '<a href="eventDetails.php?id='.$row["id_event"].'">';
-                        echo '<div class="col-lg-4 col-md-6 mb-4">';
-                        echo '<div class="card h-100" id="event">';
-                        echo '<div class="card-header justify-content-center">';
-                        echo '<a class="stretched-link text-center" href="eventDetails.php?id='.$row["id_event"].'" class="card_title">';
-                        echo '<h2 class="card-title text-center" id="nama_event' . $row['id_event'] . '">' . $row['nama_event'] . '</h2></a></div>';
-                        //echo '<a href="detailEvent.php?id='". $row["id_event"].'&nama_event='.$_SESSION['nama_event'].'"></a>';
-                        // card body
-                        echo '<div class="card-body">';
-                        // Nama Event
-                       // echo '<h2 class="card-title text-center" id="nama_event' . $row['id_event'] . '">' . $row['nama_event'] . '</h2>';
-                        // tanggal event
-                        echo '<h5 class="card-title text-center" id="tgl_event' . $row['id_event'] . '">Date: ' . $row['tanggal_event'] . '</h5>';
-                        // Lokasi event
-                        $simpanId = $row['id_lokasi'];
-                        $lokasi = "SELECT nama_lokasi FROM lokasi WHERE id_lokasi=$simpanId";
-                        $simpan = mysqli_query($conn,$lokasi);
-                        $hasil = $simpan->fetch_assoc();
-                        $hasil = $hasil['nama_lokasi'];
-                        echo '<p class="card-title text-center" id="lokasi_event' . $row['id_event'] . '">Lokasi : '. $hasil . '</p>';
-                        echo '</div>';
-                        echo '<a href="eventDetails.php?eventId='.$row["id_event"].'" class="stretched-link"></a>';
-                    // Closing div
-                        echo '</div></div>';
-                    } 
-                
+            while ($row = mysqli_fetch_array($result)) {
+                //echo '<a type="button" class="btn stretched-link" id="addToCartButton" href="details.php?id='. $row["id"].'&rest_name='.$_SESSION['rest_name'].'">+ Add to cart</a>';
+
+                //echo '<a href="eventDetails.php?id='.$row["id_event"].'">';
+                echo '<div class="col-lg-4 col-md-6 mb-4">';
+                echo '<div class="card h-100" id="event">';
+                echo '<div class="card-header justify-content-center">';
+                echo '<a class="stretched-link text-center" href="eventDetails.php?id=' . $row["id_event"] . '" class="card_title">';
+                echo '<h2 class="card-title text-center" id="nama_event' . $row['id_event'] . '">' . $row['nama_event'] . '</h2></a></div>';
+                //echo '<a href="detailEvent.php?id='". $row["id_event"].'&nama_event='.$_SESSION['nama_event'].'"></a>';
+                // card body
+                echo '<div class="card-body">';
+                // Nama Event
+                // echo '<h2 class="card-title text-center" id="nama_event' . $row['id_event'] . '">' . $row['nama_event'] . '</h2>';
+                // tanggal event
+                echo '<h5 class="card-title text-center" id="tgl_event' . $row['id_event'] . '">Date: ' . $row['tanggal_event'] . '</h5>';
+                // Lokasi event
+                $simpanId = $row['id_lokasi'];
+                $lokasi = "SELECT nama_lokasi FROM lokasi WHERE id_lokasi=$simpanId";
+                $simpan = mysqli_query($conn, $lokasi);
+                $hasil = $simpan->fetch_assoc();
+                $hasil = $hasil['nama_lokasi'];
+                echo '<p class="card-title text-center" id="lokasi_event' . $row['id_event'] . '">Lokasi : ' . $hasil . '</p>';
+                echo '</div>';
+                echo '<a href="eventDetails.php?eventId=' . $row["id_event"] . '" class="stretched-link"></a>';
+                // Closing div
+                echo '</div></div>';
+            }
+
             ?>
         </div>
-        </div>
-    
-        
-    </body>
+    </div>
+
+
+</body>
---
+++ usernavbar.php
@@ -0,0 +1,80 @@
+<?php
+session_start();
+
+?>
+<style>
+    .navbar{
+        background-color: #880015;
+        font-size: large;
+        color: white;
+        white-space: nowrap;
+        transition: .8s;
+        padding: .5rem;
+    }
+    li {
+        float: left;
+        color: #E3E2E2 !important;
+    }
+
+    li a, .dropbtn {
+       display: inline-block;
+       color: white;
+       text-align: center;
+       padding: 14px 16px;
+       text-decoration: none;
+   }
+
+    li a:hover, .dropdown:hover .dropbtn {
+       color: #FFFFFF;
+   }
+
+    li.dropdown {
+       display: inline-block;
+   }
+
+   .dropdown-content {
+      display: none;
+      position: absolute;
+      background-color: #f9f9f9;
+      min-width: 160px;
+      box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+      z-index: 1;
+   }
+
+   .dropdown-content a {
+     color: #1f302e;
+     padding: 12px 16px;
+     text-decoration: none;
+     display: block;
+     text-align: left;
+   }
+
+   .dropdown-content a:hover {background-color: #f1f1f1;}
+
+   .dropdown:hover .dropdown-content {
+     display: block;
+   }
+
+</style>
+<nav class="navbar navbar-expand-xl navbar-light px-3 fixed-top" style="width: 100%;">
+<!-- <img src="http://bem.petra.ac.id/spetrakuler/assets/images/Logo.png" width="auto" height="35" class="d-inline-block align-top" alt=""> -->
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <a class="dropbtn" aria-current="page" href="#">Home</a>
+          </li> 
+          <li class="nav-item">
+            <a class="dropbtn" aria-current="page" href="displayAllEvent.php">Explore Events</a>
+          </li>
+          <li class="nav-item">
+            <a class="dropbtn" href="">Welcome, <?php echo $_SESSION["name"] ?></a>
+          </li>
+          <li class="nav-item">
+            <a class="dropbtn" href="logout.php"><i class="bi bi-box-arrow-right"></i></a>
+          </li>
+        </ul>
+    </div>
+</nav>
