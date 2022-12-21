7916e79c08d51f98e9d51f14bbb0927681a0d654
reginafelia <116373679+reginafelia@users.noreply.github.com>
2022-12-22 01:07:33 +0700

    Update insertevent.php

--- insertevent.php
+++ insertevent.php
@@ -13,7 +13,7 @@ include 'links.php'
     <link rel="shortcut icon" href="assets/logo.png" type="image/x-icon">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="style.css">
-    <title>Backend | Add Menu</title>
+
 </head>
 
 <body style="font-family: 'Poppins', sans-serif; font-size: 20px;">
