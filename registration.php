db94517d1ab1f58bf7c584b204399e7ae604c95e
gracenatasha <c14200021@john.petra.ac.id>
2023-01-17 15:13:50 +0700

    a

--- registration.php
+++ registration.php
@@ -55,6 +55,7 @@ if (isset($_POST['data_json']) && $_POST['data_json'] == 'kelurahan') {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.min.css'>
     <link rel='stylesheet' href=https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css>
+    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 
 <body>
